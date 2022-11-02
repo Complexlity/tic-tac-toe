84f26d97090f776b9b351314b9ed12d2f1cc7b60
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-11-03 00:50:08 +0100

    Add Dom button and winning message manipulation

--- js/main.js
+++ js/main.js
@@ -10,10 +10,13 @@
 let c = console.log.bind(document)
 let dq = document.querySelector.bind(document)
 let dqa = document.querySelectorAll.bind(document)
+let gameStarted = true
 
 let gameboard = (function(){
     let player = 'X'
     let board = ['','','','','','','','','']
+    let gameOn = true
+    let winner = undefined
     let winningSequence = [
         [0,1,2], [3,4,5], [6,7,8], [0,3,6],
         [1,4,7], [2,5,8], [0,4,8],  [2,4,6] 
@@ -21,7 +24,6 @@ let gameboard = (function(){
    
     // TODO
     function switchPlayer(){
-        console.log(player)
         if(player == 'X'){
             player = 'O'
             return
@@ -31,6 +33,7 @@ let gameboard = (function(){
     
     function populate(square){
         let index = square.dataset.index
+        
         if(!board[index]){
             board[index] =  player
             square.textContent = player
@@ -45,18 +48,80 @@ let gameboard = (function(){
         return arr.every((value) => board[value] == player)
     }
 
-
-    function makeMove(square){
-        populate(square)
-        switchPlayer()
+    function checkGame(){
+        winner = undefined
+        gameOn = true
+        if(checkSequence()){
+            winner = player
+            gameOn = false 
+            board.forEach(value => value = '')
+            
+        }
+        else if (board.every(value => value == true)){
+            gameOn = false
+            board.forEach(value => value = '')
+        }
+        return {winner, gameOn}
     }
 
-    return {makeMove, board, checkSequence, isValidSequence}
+function makeMove(square){
+    populate(square)
+    let winningCheck = checkGame()
+    if(!gameOn){
+        let winner = winningCheck.winner
+        if(winningCheck.winner){
+            result = `${winner} wins`
+        }
+        else result = "it's a draw"
+        
+        resetDefaults()
+        return result
+    }
+    else{
+        switchPlayer()
+        return false
+    }
+}
+
+function resetDefaults(){
+    player = "X"
+    winner = undefined
+    board = ['','','','','','','','','']
+}
+
+    return {makeMove, checkGame, switchPlayer, resetDefaults}
 
 })()
     
-let squares = dqa('.tiles')
+let squares = dqa('[data-index]')
+let start = dq('.start')
+let message = dq('.message')
+
 squares.forEach(square => {
-    square.addEventListener('click', (e) => gameboard.makeMove(e.target))
+    square.addEventListener('click', playGame)
 })
+start.addEventListener('click', startGame)
+
+function playGame(){
+    if (gameStarted){
+        let gameResult = gameboard.makeMove(this)
+        // c(message)
+        if(gameResult) {
+            message.style.display='block'
+            message.textContent = gameResult
+            gameStarted = false 
+            start.style.display = 'block' 
+        }
+}
+}
+
+function startGame(){
+    gameStarted = true
+    this.style.display = 'none';
+    squares.forEach(square => {
+        square.textContent = ''
+    })
+    message.style.display = 'none'
+
+}
 
